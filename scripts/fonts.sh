c790c3dba4218c95a0b443f5cabcd98a98299f8d
Hugo Guimarães <50011557+Hugomguima@users.noreply.github.com>
2022-05-11 13:16:11 +0100

    Added font configuration script

---
+++ scripts/fonts.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+## File for configuring the fonts for the computer
+
+### Add noto-fonts to allow special unicode characters and emojis
+yay -S noto-fonts noto-fonts emoji
+
+### Add japanese fonts
+yay -S adobe-source-han-sans-jp-fonts
