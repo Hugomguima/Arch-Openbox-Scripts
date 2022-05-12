58ac2c26b226c12b20d04ea1f5440c47322e2065
Hugo Guimarães <50011557+Hugomguima@users.noreply.github.com>
2022-05-12 21:32:17 +0100

    Added --noconfirm flag to ease instalation

--- scripts/aur.sh
+++ scripts/aur.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 ### Updating the system
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 
 ### Installing yay as a package manager helper
-sudo pacman -S yay
+sudo pacman -S yay --noconfirm
 
 ### Packages that can be installed by pacman
-yay -S discord caprine spotify code flameshot github-desktop stremio libreoffice
+yay -S discord caprine spotify code flameshot github-desktop stremio libreoffice --noconfirm
 
 ### Packages from the AUR
-yay -S google-chrome whatsapp-for-linux snapd qtws-base python-pulsectl caffeine-ng
+yay -S google-chrome whatsapp-for-linux snapd qtws-base python-pulsectl caffeine-ng --noconfirm
--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -3,7 +3,7 @@
 ## File for configuring the fonts for the computer
 
 ### Add noto-fonts to allow special unicode characters and emojis
-yay -S noto-fonts noto-fonts emoji
+yay -S noto-fonts noto-fonts emoji --noconfirm
 
 ### Add japanese fonts
-yay -S adobe-source-han-sans-jp-fonts
+yay -S adobe-source-han-sans-jp-fonts --noconfirm
