a91095faeeaa006aed02ae042cbef77c2599cb9e
hugomguima <hugomguima@gmail.com>
2022-05-30 10:24:47 +0100

    hello

--- scripts/files.sh
+++ scripts/files.sh
@@ -8,3 +8,14 @@ cp ../files/rc.xml ~/.config/openbox/rc.xml
 
 ### Config terminal settings (only opacity by now)
 cp ../files/alacritty.yml ~/.config/alacritty.yml
+
+### File with custom aliases
+cp ../files/zshrc ~/.zshrc
+
+### Restart openbox configurations
+openbox --restart
+
+### Apply alias configurations
+source ~/.zshrc
+
+
